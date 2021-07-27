4bb8298ccd774a435ac82193483f5e418acb0fd6
nikolaypleshkov <icelol@abv.bg>
2021-07-27 16:52:59 +0300

    Slideshow component

---
+++ src/components/Slideshow/slideshow-component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './Slideshow.css'
+export default class Slideshow extends Component {
+    render() {
+        return (
+            <div className="sls-container">
+                <h1>Slideshow</h1>
+            </div>
+        )
+    }
+}
--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar/navbar-component'
+import Slideshow from '../components/Slideshow/slideshow-component'
 //test
 function Home() {
     return (
         <>
             <Navbar />
+            <Slideshow />
 
         </>
     )
