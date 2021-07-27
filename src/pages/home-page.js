f11f60be71f5b50f178dba28dea0ba702d007eb9
nikolaypleshkov <icelol@abv.bg>
2021-07-27 16:35:16 +0300

    moved sidebar to Navbar Component

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import Navbar from '../components/Navbar/navbar-component'
-import Sidebar from '../components/Sidebar/sidebar-components'
 //test
 function Home() {
     return (
         <>
             <Navbar />
-            <div>
-             <Sidebar /> 
-            </div>
 
         </>
     )
