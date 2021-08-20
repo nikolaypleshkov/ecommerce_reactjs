ac854af225dbcb3bcf0e8c0857439a2f36119a75
Nikolay Pleshkov <icelol@abv.bg>
2021-08-20 17:20:46 +0300

    Creating layout for Order summary

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Typography, Button, Divider } from '@material-ui/core'
+import { Elements, CardElement, ElementConsumer } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
 
-const PaymentForm = () => {
+import Review from './Review.jsx';
+const PaymentForm = ( { checkoutToken }) => {
     return (
-        <div>
-            Payment Form      
-        </div>
+        <>
+           <Review checkoutToken={checkoutToken} />
+        </>
     )
 }
 
